33b03966feca7ae743cf24841424b91dd9ab65d9
LeidyRomero <lj.romero@uniandes.edu.co>
2019-04-25 17:36:10 -0500

    identacion

--- reportes/views.py
+++ reportes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from .forms import ReporteForm
 from .models import Reporte
@@ -8,28 +8,32 @@ import hashlib
 import base64
 import requests
 
+
 def registrar(request):
-#    hashLocal = hashlib.new("sha256", request)
-#   digestLocal = hashLocal.digest()
-#   if digestLocal == request:
-        form = ReporteForm(request.POST or None)
-        if form.is_valid():
-            hash1 = form.cleaned_data['hash']
-	    form.save()
-            form = ReporteForm()
+    #    hashLocal = hashlib.new("sha256", request)
+    #   digestLocal = hashLocal.digest()
+    #   if digestLocal == request:
+    form = ReporteForm(request.POST or None)
+    if form.is_valid():
+        hash1 = form.cleaned_data['hash']
+        form.save()
+        form = ReporteForm()
         context = {
             'form': form,
         }
         return render(request, 'Post_reportes.html', context)
+
+
 #    else:
 #        return HttpResponse("Erro")
 
 def index(request):
     return render(request, 'base.html', {})
 
+
 def listarReportes(request):
     queryset = Reporte.objects.all()
     context = {
-        'reporte_list' : queryset
+        'reporte_list': queryset
     }
     return render(request, 'List_Reportes.html', context)
